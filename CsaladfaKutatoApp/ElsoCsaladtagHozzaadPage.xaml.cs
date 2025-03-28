409df9a505742e728d93d96631ad9251fe6d3242
kovacskrissz2001 <kivikovi06@gmail.com>
2025-03-28 16:51:59 +0100

    Első családtag hozzáadása oldal UI kész.

---
+++ CsaladfaKutatoApp/ElsoCsaladtagHozzaadPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MahApps.Metro.Controls;
+
+namespace CsaladfaKutatoApp
+{
+    /// <summary>
+    /// Interaction logic for ElsoCsaladtagHozzaadPage.xaml
+    /// </summary>
+    public partial class ElsoCsaladtagHozzaadPage : Page
+    {
+        public ElsoCsaladtagHozzaadPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Regisztracio_Click(object sender, RoutedEventArgs e)
+        {
+
+            // A "MainWindow" Frame-jén keresztül navigálunk
+            ((MainWindow)System.Windows.Application.Current.MainWindow).MainFrame.Navigate(new RegisztracioPage());
+            e.Handled = true;
+        }
+    }
+}
